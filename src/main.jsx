239c314db48d2f7f4e7f26388f9ec1754fbc57a2
anand13081995 <anandsahani213@gmail.com>
2024-05-04 16:28:09 +0530

    icons as png by react learned

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import IconsPart1 from './components/IconsPart1/IconsPart1'
+import IconsPart2 from './components/IconsPart2/IconsPart2'
+import IconsPart3 from './components/IconsPart3/IconsPart3'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IconsPart1 />
+    <IconsPart2 />
+    <IconsPart3 />
+  </React.StrictMode>,
+)
